628da6e4f9e9ffb1d81404d49a983bd91235e691
Altan Ali Ejder <altanalie@gmail.com>
2021-08-02 12:58:52 +0300

    Add project files.

---
+++ WebAPI/Controllers/TestsController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        ITestService _testService;
+
+        public TestsController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        [HttpGet("gettestmethod")]
+        public string GetTestMethod()
+        {
+            return _testService.TestMethod();
+        }
+
+        [HttpGet("gethelloworld")]
+        public string GetHelloWorld()
+        {
+            return _testService.HelloWorld();
+        }
+    }
+}
